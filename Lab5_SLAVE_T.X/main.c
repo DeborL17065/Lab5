75c563898b44a1bac99e6e9fb9a7bd93dadf23c3
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-02-23 00:06:03 -0600

    Update main.c
    I2C SLAVE CONFIGURADO

--- Lab5_SLAVE_T.X/main.c
+++ Lab5_SLAVE_T.X/main.c
@@ -31,9 +31,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "I2C.h"
 
 
 void init(void);
+
+uint8_t z;
+uint8_t dato;
+
+//*****************************************************************************
+// Código de Interrupción 
+//*****************************************************************************
+void __interrupt() isr(void){
+   if(PIR1bits.SSPIF == 1){ 
+
+        SSPCONbits.CKP = 0;
+       
+        if ((SSPCONbits.SSPOV) || (SSPCONbits.WCOL)){
+            z = SSPBUF;                 // Read the previous value to clear the buffer
+            SSPCONbits.SSPOV = 0;       // Clear the overflow flag
+            SSPCONbits.WCOL = 0;        // Clear the collision bit
+            SSPCONbits.CKP = 1;         // Enables SCL (Clock)
+        }
+
+        if(!SSPSTATbits.D_nA && !SSPSTATbits.R_nW) {
+            //__delay_us(7);
+            z = SSPBUF;                 // Lectura del SSBUF para limpiar el buffer y la bandera BF
+            //__delay_us(2);
+            PIR1bits.SSPIF = 0;         // Limpia bandera de interrupción recepción/transmisión SSP
+            SSPCONbits.CKP = 1;         // Habilita entrada de pulsos de reloj SCL
+            while(!SSPSTATbits.BF);     // Esperar a que la recepción se complete
+////            PORTD = SSPBUF;             // Guardar en el PORTD el valor del buffer de recepción
+////            __delay_us(250);
+            
+        }else if(!SSPSTATbits.D_nA && SSPSTATbits.R_nW){
+            z = SSPBUF;
+            BF = 0;
+            SSPBUF = PORTB;
+            SSPCONbits.CKP = 1;
+            __delay_us(250);
+            while(SSPSTATbits.BF);
+        }
+       
+        PIR1bits.SSPIF = 0;    
+    }
+}
+
 
 void main(void) {
     init();
@@ -57,7 +100,9 @@ void init(void) {
     PORTD =0;          //se limpia el puerto D
     PORTA =0;          //se limpia el puerto A
     //////////////////////////////////////////////////////////
+    ANSEL = 0;
     ANSELH =0;
+   // I2C_Slave_Init(0x50); 
 //    ANSELbits.ANS0 =1; //RA0 ANALóGICO
 //    ANSELbits.ANS1 =1; //RA1 ANALóGICO  
 }
