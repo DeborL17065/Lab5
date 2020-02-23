54742820837a9683e8ce6d50bd37967cb4e59596
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-02-22 21:00:25 -0600

    Create I2C.c

---
+++ Lab5_MASTER.X/I2C.c
@@ -0,0 +1,17 @@
+/*
+ * File:   I2C.c
+ * Author: debor
+ *
+ * Created on February 22, 2020, 8:36 PM
+ */
+
+
+#define _XTAL_FREQ   8000000
+#include <xc.h>
+#include <pic16f887.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
+
